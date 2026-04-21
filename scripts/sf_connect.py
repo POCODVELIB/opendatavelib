fa9e222542f0e9a992fe52a32c942a7dbfe7f03b
Said HOUSSEINE <said.housseine@talan.com>
2026-04-21 16:44:17 +0200

    edit header

--- scripts/sf_connect.py
+++ scripts/sf_connect.py
@@ -1,3 +1,11 @@
+"""
+Description :  POC ingestion de données météo à Paris dans Snowflake
+-------------------------------------------------------------------------------------
+Author      : Said HOUSSEINE   
+Created     : 2026-04-21
+-------------------------------------------------------------------------------------
+"""
+
 import os, snowflake.connector
 
 def get_connection():
